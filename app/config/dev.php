3b81f76680ad9c72707568b8f680d8cb093bab5f
jeapy <raabduhamel@ymail.com>
2017-08-02 13:44:03 +0000

    Update dev.php

--- app/config/dev.php
+++ app/config/dev.php
@@ -8,13 +8,3 @@ $app['debug'] = true;
 $app['monolog.level'] = 'INFO';
 
 
-// Doctrine (db)
-$app['db.options'] = array(
-    'driver'   => 'pdo_mysql',
-    'host'     => '127.0.0.1',
-    'port'     => '3306',
-    'dbname'   => 'scimap',
-    'user'     => 'root',
-    'password' => '',
-);
-
